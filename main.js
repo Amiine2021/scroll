f0e16bbd5fa76f31008ad8d20ed4ae37e6f19063
amine <mraminewiebke@gmail.com>
2023-09-07 14:10:27 +0200

    scroll button done work well

---
+++ main.js
@@ -0,0 +1,19 @@
+let btn = document.getElementById('btn');
+
+
+onscroll = function(){
+    if(this.scrollY > 400){
+        btn.style.display = 'block'
+    } else {
+        btn.style.display = 'none'
+    }
+}
+
+this.onclick = function(){
+
+    scroll({
+        left:0,
+        top:0,
+        behavior: 'smooth'
+    })
+}
